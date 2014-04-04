b02d1adb17c72941d0cdc0c19fa589fc00370dfc
Leonardo Souza <lsouza@santillana.com>
2014-04-04 11:15:48 -0300

    Retirado os menus na index da categoria Incluido o encode = false do drop down list

--- protected/views/categoria/index.php
+++ protected/views/categoria/index.php
@@ -5,51 +5,11 @@ $this->breadcrumbs = array(
 	Yii::t('app', 'Index'),
 );
 
-$this->menu = array(
-	array('label'=>Yii::t('app', 'Create') . ' ' . Categoria::label(), 'url' => array('create')),
-	array('label'=>Yii::t('app', 'Manage') . ' ' . Categoria::label(2), 'url' => array('admin')),
-);
 ?>
 
 <h1><?php echo GxHtml::encode(Categoria::label(2)); ?></h1>
 
 <?php
-Categoria::getChildren();exit;
-echo CHtml::dropDownList('teste', '', Categoria::getCategoriaOptions());
-/* $this->widget('zii.widgets.CListView', array(
-	'dataProvider'=>$dataProvider,
-	'itemView'=>'_view',
-)); */
-/*$level=0;
-$categories = Categoria::model()->findAll(array('order'=>'lft'));
-foreach($categories as $n=>$category)
-{
-    if($category->lvl==$level)
-        echo CHtml::closeTag('li')."\n";
-    else if($category->lvl>$level)
-        echo CHtml::openTag('ul')."\n";
-    else
-    {
-        echo CHtml::closeTag('li')."\n";
- 
-        for($i=$level-$model->lvl;$i;$i--)
-        {
-            echo CHtml::closeTag('ul')."\n";
-            echo CHtml::closeTag('li')."\n";
-        }
-    }
- 
-    echo CHtml::openTag('li');
-    echo CHtml::encode($category->nome);
-    $level=$category->lvl;
-}
- 
-for($i=$level;$i;$i--)
-{
-    echo CHtml::closeTag('li')."\n";
-    echo CHtml::closeTag('ul')."\n";
-}*/
-
 
 /**
  * tree view  file
@@ -69,4 +29,5 @@ $this->widget('application.widgets.JsTreeWidget',
         'jstree_container_ID' => 'Categoria-wrapper',
         'themes' => array('theme' => 'default', 'dots' => true, 'icons' => true),
         'plugins' => array('themes', 'html_data', 'contextmenu', 'crrm', 'dnd', 'cookies', 'ui')
-    ));
+    )
+);
