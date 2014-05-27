ed7a51d90e710ca74097b391721a2adc89953dc7
Leonardo Souza <leogsouza@gmail.com>
2014-05-26 23:33:18 -0300

    Alteração da criação da url de logout

--- protected/views/layouts/main.php
+++ protected/views/layouts/main.php
@@ -521,7 +521,7 @@ License: You must have a valid license purchased only from themeforest(the above
                                 </a>
                             </li>
                             <li>
-                                <a href="<?php echo $this->createUrl('site/logout');?>">
+                                <a href="<?php echo $this->createUrl('/site/logout');?>">
                                     <i class="fa fa-key"></i> Log Out
                                 </a>
                             </li>
